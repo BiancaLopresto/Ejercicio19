09a21e9d75bd0c911502d76ce1d654e262c3d00b
BiancaLopresto <bianlopre19@gmail.com>
2023-05-27 00:36:58 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio19.Consola/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejercicio19.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Celsius;
+            double Reaumur;
+            double Fahrenheit;
+
+            Console.Write("Ingrese los grados Celsius: ");
+            Celsius=int.Parse(Console.ReadLine());
+
+            if (Celsius>30)
+            {
+                Reaumur = 0.8 * Celsius;
+                Console.Write($"Los grados Celsius {Celsius} convertidos a Reaumur son: {Reaumur} grados");
+            }
+            else
+            {
+                Fahrenheit = 1.8 * Celsius + 32;
+                Console.Write($"Los grados Celsius {Celsius} convertidos a Fahrenheit son: {Fahrenheit} grados");
+                
+            }
+
+        }
+    }
+}
